4c5d08a86f34f2ad4509ad9a3c211853ae5f3079
Sebastian Milkowski <sebastianmilkowski@MacBook-Pro-2.local>
2020-05-19 18:36:10 -0700

    All changes for part 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,31 @@
-console.log('Hello from js/scripts.js!');
+
+const displayedImage = document.querySelector('.displayed-img');
+const thumBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+
+  for (let i = 1; i <= 5; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'image/pic' +i+ '.jpg');
+    thumBar.appendChild(newImage);
+    newImage.onclick = function(e) {
+      displayedImage.src = e.target.src;
+    }
+}
+
+
+
+btn.onclick = function() {
+  const btnClass = btn.getAttribute('class');
+  if(btnClass === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+}
